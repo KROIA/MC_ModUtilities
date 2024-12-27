5951745c33250b81debc0fd2f53d77c8f415d689
Alex <alexkrieg@gmx.ch>
2024-12-27 23:11:41 +0100

    -

---
+++ src/main/java/net/kroia/modutilities/gui/elements/ItemView.java
@@ -0,0 +1,45 @@
+package net.kroia.modutilities.gui.elements;
+
+import net.kroia.modutilities.gui.elements.base.GuiElement;
+import net.kroia.modutilities.gui.geometry.Point;
+import net.minecraft.world.item.ItemStack;
+
+public class ItemView extends GuiElement {
+    public static final int DEFAULT_WIDTH = 16;
+
+    ItemStack itemStack;
+    Point itemPos = new Point(0,0);
+    public ItemView() {
+        super();
+    }
+    public ItemView(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    public ItemView(ItemStack itemStack) {
+        super();
+        this.itemStack = itemStack;
+    }
+    public ItemView(int x, int y, int width, int height, ItemStack itemStack) {
+        super(x, y, width, height);
+        this.itemStack = itemStack;
+    }
+    public void setItemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+
+    @Override
+    protected void render() {
+        if(itemStack == null)
+            return;
+
+        drawItem(itemStack, itemPos);
+    }
+
+    @Override
+    protected void layoutChanged() {
+        itemPos.x = (getWidth() - DEFAULT_WIDTH) / 2;
+        itemPos.y = (getHeight() - DEFAULT_WIDTH) / 2;
+    }
+}
