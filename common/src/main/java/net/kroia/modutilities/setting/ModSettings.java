57ea064c25111cc6fadb33a0dc1f0c1783c35101
Alex <alexkrieg@gmx.ch>
2025-08-18 21:14:19 +0200

    ~ replaced ModSettings info logger by debug logger

--- common/src/main/java/net/kroia/modutilities/setting/ModSettings.java
+++ common/src/main/java/net/kroia/modutilities/setting/ModSettings.java
@@ -1,8 +1,6 @@
 package net.kroia.modutilities.setting;
 
 
-import net.minecraft.world.item.enchantment.ThornsEnchantment;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +15,7 @@ public class ModSettings {
             errorLogger.accept(error + "\nError: " + throwable.getMessage() + "\n" + Arrays.toString(throwable.getStackTrace()));
         }
     };
-    public Consumer<String> infoLogger = System.out::println;
+    public Consumer<String> debugLogger = System.out::println;
     private final List<SettingsGroup> allGroups = new ArrayList<>();
     //private String settingsFileName;
     private final String name;
@@ -26,14 +24,14 @@ public class ModSettings {
         this.name = name;
     }
 
-    public void setLogger(Consumer<String> errorLogger, Consumer<String> infoLogger) {
+    public void setLogger(Consumer<String> errorLogger, Consumer<String> debugLogger) {
         this.errorLogger = errorLogger;
-        this.infoLogger = infoLogger;
+        this.debugLogger = debugLogger;
     }
-    public void setLogger(Consumer<String> errorLogger, BiConsumer<String, Throwable>errorLoggerThrowable, Consumer<String> infoLogger) {
+    public void setLogger(Consumer<String> errorLogger, BiConsumer<String, Throwable>errorLoggerThrowable, Consumer<String> debugLogger) {
         this.errorLogger = errorLogger;
         this.errorLoggerThrowable = errorLoggerThrowable;
-        this.infoLogger = infoLogger;
+        this.debugLogger = debugLogger;
     }
 
     protected <T extends SettingsGroup> T createGroup(T group) {
@@ -53,8 +51,8 @@ public class ModSettings {
                 errorLoggerThrowable.accept("Failed to save "+name+" to path: " + filePath + "\nError:\n" + e.getMessage(), e);
             return false;
         }
-        if(infoLogger != null)
-            infoLogger.accept(name+" saved to JSON file: " + filePath);
+        if(debugLogger != null)
+            debugLogger.accept(name+" saved to JSON file: " + filePath);
         return true;
     }
     public boolean loadSettings(String filePath)
@@ -67,8 +65,8 @@ public class ModSettings {
                 errorLoggerThrowable.accept("Failed to load "+name+" from path: " +filePath + "\nError:\n" + e.getMessage(), e);
             return false;
         }
-        if(infoLogger != null)
-            infoLogger.accept(name+" loaded from JSON file: " + filePath);
+        if(debugLogger != null)
+            debugLogger.accept(name+" loaded from JSON file: " + filePath);
         return true;
     }
 }
