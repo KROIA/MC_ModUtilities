f8dd7a608e031b8f003a669c4a4958ee077ac349
Alex <alexkrieg@gmx.ch>
2025-08-21 02:14:59 +0200

    ~Sandbox

--- common/src/main/java/net/kroia/modutilities/sandbox/Sandbox.java
+++ common/src/main/java/net/kroia/modutilities/sandbox/Sandbox.java
@@ -6,12 +6,9 @@ import dev.architectury.event.events.common.CommandRegistrationEvent;
 import net.kroia.modutilities.JsonUtilities;
 import net.kroia.modutilities.ModUtilitiesMod;
 import net.kroia.modutilities.networking.NetworkManager;
-import net.kroia.modutilities.networking.NetworkPacket;
 import net.kroia.modutilities.setting.parser.ItemStackJsonParser;
-import net.minecraft.client.Minecraft;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
@@ -21,18 +18,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Sandbox {
-    private static class SandboxNetwork extends NetworkManager
+    public static class SandboxNetwork extends NetworkManager
     {
         public SandboxNetwork() {
             super(ModUtilitiesMod.MOD_ID);
             setupClientReceiverPackets();
             setupServerReceiverPackets();
             this.setupARRS(); // Setup the Asynchronous Request Response System (ARRS)
+
+            SandboxOpenGuiPacket.network = this; // Set the network instance for the SandboxOpenGuiPacket
         }
 
         @Override
         public void setupClientReceiverPackets() {
-            register(SandboxOpenGuiPacked.class, SandboxOpenGuiPacked::encode, SandboxOpenGuiPacked::new, SandboxOpenGuiPacked::receive);
+            register(SandboxOpenGuiPacket.class, SandboxOpenGuiPacket::encode, SandboxOpenGuiPacket::new, SandboxOpenGuiPacket::receive);
 
         }
 
@@ -42,61 +41,16 @@ public class Sandbox {
         }
     }
 
-    private static class SandboxOpenGuiPacked extends NetworkPacket {
-
-        enum GuiType {
-            TEST_SCREEN,
-            ANOTHER_SCREEN
-        }
-        private GuiType guiType;
-        public SandboxOpenGuiPacked(GuiType guiType) {
-            this.guiType = guiType;
-        }
-        public SandboxOpenGuiPacked() {
-            super();
-        }
-        public SandboxOpenGuiPacked(FriendlyByteBuf friendlyByteBuf) {
-            super(friendlyByteBuf);
-        }
-
-        public static void send(ServerPlayer receiver, GuiType guiType) {
-            SandboxOpenGuiPacked packet = new SandboxOpenGuiPacked(guiType);
-            network.sendToClient(receiver, packet);
-        }
-        @Override
-        public void decode(FriendlyByteBuf buf) {
-            int ordinal = buf.readInt();
-            this.guiType = GuiType.values()[ordinal];
-        }
-
-        @Override
-        public void encode(FriendlyByteBuf buf) {
-            buf.writeInt(guiType.ordinal());
-        }
-
-        @Override
-        protected void handleOnClient() {
-            switch(guiType) {
-                case TEST_SCREEN:
-                    Minecraft.getInstance().setScreen(new TestScreen());
-                    break;
-                case ANOTHER_SCREEN:
-                    // Open another GUI
-                    break;
-                default:
-            }
-        }
-    }
-
     private static final class SandboxCommand
     {
         public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
             dispatcher.register(
                     Commands.literal("modutilities")
+                            .requires(source -> source.hasPermission(2)) // Requires OP permission level
                             .then(Commands.literal("openTestScreen")
                                     .executes(context -> {
                                         ServerPlayer player = context.getSource().getPlayerOrException();
-                                        SandboxOpenGuiPacked.send(player, SandboxOpenGuiPacked.GuiType.TEST_SCREEN);
+                                        SandboxOpenGuiPacket.send(player, SandboxOpenGuiPacket.GuiType.TEST_SCREEN);
                                         return 1;
                                     }))
                             .then(Commands.literal("saveItemInHand")
